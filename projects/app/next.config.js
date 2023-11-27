ca7d7f12fb3b83ced679f45e94a12e44308f8332
jiangtong <jiangtong@asisinfo.com>
2023-11-27 13:52:05 +0800

    new

--- projects/app/next.config.js
+++ projects/app/next.config.js
@@ -7,6 +7,16 @@ const nextConfig = {
   output: 'standalone',
   reactStrictMode: process.env.NODE_ENV === 'development' ? false : true,
   compress: true,
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'https://10.1.251.110:8090/api/:path*',
+        // source: '/api',
+        // destination: 'https://10.1.251.110:8090', // 将请求代理到目标URL 67727a41b5b1d4dfca981e4045b1bb2f1e7fef0e3e8825c028949d186cad4c00
+      },
+    ];
+  },
   webpack(config, { isServer }) {
     if (!isServer) {
       config.resolve = {
