321b8c794b8682d16b7f3141988ab448f39db21b
jiangtong <jiangtong@asisinfo.com>
2023-12-19 14:57:18 +0800

    sigin

--- projects/app/src/pages/signin/index.tsx
+++ projects/app/src/pages/signin/index.tsx
@@ -88,6 +88,7 @@ const Login = () => {
     clearToken();
       const urlParams = new URLSearchParams(window.location.search);
       const token = urlParams.get('token');
+      const path = urlParams.get('path');
 
       if (token) {
 
@@ -114,9 +115,30 @@ const Login = () => {
 
               setUserInfo(res.user);
               setToken(res.token);
-              setTimeout(() => {
-                  router.push('/app/list');
-              }, 300);
+              if (path) {
+                  if (path === 'app') {
+                      setTimeout(() => {
+                          router.push('/app/list');
+                      }, 300);
+                  }else if(path === 'dataset'){
+                      setTimeout(() => {
+                          router.push('/dataset/list');
+                      }, 300);
+                  }else if(path === 'chat'){
+                      setTimeout(() => {
+                          router.push('/chat');
+                      }, 300);
+                  }else {
+                      setTimeout(() => {
+                          router.push('/app/list');
+                      }, 300);
+                  }
+              }else {
+                  setTimeout(() => {
+                      router.push('/app/list');
+                  }, 300);
+              }
+
               // router.replace('/app/list');
           }).catch(function (error){
               toast({
@@ -136,96 +158,7 @@ const Login = () => {
       <Box ref={Dom} w={'100%'} flex={'1 0 0'}  h={'100%'} position={'relative'}>
           <Loading loading={true} fixed={true} />
       </Box>
-    // <>
-    //   {feConfigs.googleClientVerKey && (
-    //     <Script
-    //       src={`https://www.recaptcha.net/recaptcha/api.js?render=${feConfigs.googleClientVerKey}`}
-    //     ></Script>
-    //   )}
-    //   <Flex
-    //     alignItems={'center'}
-    //     justifyContent={'center'}
-    //     bg={`url('/icon/login-bg.svg') no-repeat`}
-    //     backgroundSize={'cover'}
-    //     userSelect={'none'}
-    //     h={'100%'}
-    //     px={[0, '10vw']}
-    //   >
-    //     <Flex
-    //       height="100%"
-    //       w={'100%'}
-    //       maxW={'1240px'}
-    //       maxH={['auto', 'max(660px,80vh)']}
-    //       backgroundColor={'#fff'}
-    //       alignItems={'center'}
-    //       justifyContent={'center'}
-    //       py={[5, 10]}
-    //       px={'5vw'}
-    //       borderRadius={isPc ? 'md' : 'none'}
-    //       gap={5}
-    //     >
-    //       {isPc && (
-    //         <Image
-    //           src={'/icon/loginLeft.svg'}
-    //           order={pageType === PageTypeEnum.login ? 0 : 2}
-    //           flex={'1 0 0'}
-    //           w="0"
-    //           maxW={'600px'}
-    //           height={'100%'}
-    //           maxH={'450px'}
-    //           alt=""
-    //           loading={'lazy'}
-    //         />
-    //       )}
-    //
-    //
-    //       <Box
-    //         position={'relative'}
-    //         order={1}
-    //         flex={`0 0 ${isPc ? '400px' : '100%'}`}
-    //         height={'100%'}
-    //         border="1px"
-    //         borderColor="gray.200"
-    //         display='flex'
-    //         flexDirection='column'
-    //         alignItems='center'
-    //         justifyContent='center'
-    //         py={5}
-    //         px={10}
-    //         borderRadius={isPc ? 'md' : 'none'}
-    //       >
-    //         {/*<DynamicComponent type={pageType} />*/}
-    //         {/*  <MyTooltip label={'加载中...'}>*/}
-    //         {/*      <MyIcon {...controlIconStyle} name={'common/loading'} />*/}
-    //         {/*      <Loading loading={true} zIndex={999999} />*/}
-    //         {/*     */}
-    //         {/*  </MyTooltip>*/}
-    //           <div style={{width: '100%',height:'100%'}}>
-    //               <div style={{textAlign: 'center'}}>
-    //                   <Spinner thickness="4px" speed="0.65s" emptyColor="myGray.100" color="myBlue.600" size="xl" />
-    //               </div>
-    //               <p style={{textAlign: 'center'}}>{'获取单点用户中...'}</p>
-    //           </div>
-    //
-    //         {/*{feConfigs?.concatMd && (*/}
-    //         {/*  <Box*/}
-    //         {/*    fontWeight={'bold'}*/}
-    //         {/*    color={'myBlue.700'}*/}
-    //         {/*    cursor={'pointer'}*/}
-    //         {/*    position={'absolute'}*/}
-    //         {/*    right={5}*/}
-    //         {/*    bottom={3}*/}
-    //         {/*    onClick={onOpen}*/}
-    //         {/*  >*/}
-    //         {/*    无法登录，点击联系*/}
-    //         {/*  </Box>*/}
-    //         {/*)}*/}
-    //       </Box>
-    //     </Flex>
-    //
-    //     {isOpen && <CommunityModal onClose={onClose} />}
-    //   </Flex>
-    // </>
+
   );
 };
 
